6d8665a873de5b4fb60ba3d8a2402a6a17a34e81
Viren Mirpuri <Viren389@gmail.com>
2025-10-24 11:23:14 +0000

    I want to bring my firebase project https://studio.firebase.google.com/s

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -5,7 +5,7 @@ import { TimelineScene } from '@/components/timeline-scene';
 
 export default function TimelinePage() {
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full absolute inset-0">
       <TimelineScene />
     </div>
   );
