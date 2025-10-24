027d088ca1a91777cb22a4760059f7e80d7f23f2
Viren Mirpuri <Viren389@gmail.com>
2025-10-24 21:27:56 +0000

    In the museum page, I want to make a 3d room which the users can browse

--- src/components/museum-scene.tsx
+++ src/components/museum-scene.tsx
@@ -1,10 +1,10 @@
 
 'use client';
 
-import { Canvas } from '@react-three/fiber';
-import { PointerLockControls, Text } from '@react-three/drei';
-import * as THREE from 'three';
 import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
 
 function Room() {
   const wallMaterial = new THREE.MeshStandardMaterial({ color: '#f0f0f0', side: THREE.DoubleSide });
@@ -13,24 +13,34 @@ function Room() {
   return (
     <>
       {/* Floor */}
-      <mesh rotation-x={-Math.PI / 2} position={[0, -2, 0]} material={floorMaterial}>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
         <planeGeometry args={[20, 20]} />
+        <primitive object={floorMaterial} />
       </mesh>
       {/* Ceiling */}
-      <mesh rotation-x={Math.PI / 2} position={[0, 8, 0]} material={wallMaterial}>
+      <mesh rotation={[Math.PI / 2, 0, 0]} position={[0, 8, 0]}>
         <planeGeometry args={[20, 20]} />
+        <primitive object={wallMaterial} />
       </mesh>
       {/* Back Wall */}
-      <mesh position={[0, 3, -10]} material={wallMaterial}>
+      <mesh position={[0, 3, -10]}>
         <planeGeometry args={[20, 10]} />
+        <primitive object={wallMaterial} />
+      </mesh>
+      {/* Front Wall */}
+      <mesh position={[0, 3, 10]}>
+        <planeGeometry args={[20, 10]} />
+        <primitive object={wallMaterial} />
       </mesh>
       {/* Left Wall */}
-      <mesh rotation-y={Math.PI / 2} position={[-10, 3, 0]} material={wallMaterial}>
+      <mesh rotation={[0, Math.PI / 2, 0]} position={[-10, 3, 0]}>
         <planeGeometry args={[20, 10]} />
+        <primitive object={wallMaterial} />
       </mesh>
       {/* Right Wall */}
-      <mesh rotation-y={-Math.PI / 2} position={[10, 3, 0]} material={wallMaterial}>
+      <mesh rotation={[0, -Math.PI / 2, 0]} position={[10, 3, 0]}>
         <planeGeometry args={[20, 10]} />
+        <primitive object={wallMaterial} />
       </mesh>
     </>
   );
@@ -40,19 +50,10 @@ export function MuseumScene() {
   return (
     <Suspense fallback={null}>
       <Canvas camera={{ position: [0, 1.6, 0], fov: 75 }}>
-        <ambientLight intensity={0.3} />
+        <ambientLight intensity={0.5} />
         <pointLight intensity={0.8} position={[0, 5, 0]} />
         <Room />
-        <PointerLockControls />
-        <Text
-          position={[0, 2, -9.9]}
-          fontSize={0.5}
-          color="black"
-          anchorX="center"
-          anchorY="middle"
-        >
-          Click to explore the Museum
-        </Text>
+        <OrbitControls />
       </Canvas>
     </Suspense>
   );
