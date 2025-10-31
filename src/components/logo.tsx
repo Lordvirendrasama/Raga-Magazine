ed34f13f0a0d380cd406379b9c7f7651cb6a4a4c
Viren Mirpuri <Viren389@gmail.com>
2025-10-31 22:43:37 +0000

    Raag-Projct-e1727231867617.png in public/ is my logo. make it my logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ export const Logo = ({ className }: { className?: string }) => {
   return (
     <div className={cn("relative", className)}>
         <Image
-          src="/logo.svg"
+          src="/Raag-Projct-e1727231867617.png"
           alt="Raga Logo"
           fill
           className="object-contain"
