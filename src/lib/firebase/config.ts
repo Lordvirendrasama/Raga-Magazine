d59c0657e785e7dd0425a24e8f868e236874861f
Viren Mirpuri <Viren389@gmail.com>
2025-11-01 00:08:22 +0000

    File changes

---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,13 @@
+
+'use server';
+import 'dotenv/config';
+
+const config = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    : undefined,
+};
+
+export default config;
--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -2,34 +2,23 @@
 'use server';
 
 import * as admin from 'firebase-admin';
-
-// This is a workaround for the Vercel deployment environment.
-// The private key is not correctly parsed from the .env file.
-const privateKey = process.env.FIREBASE_PRIVATE_KEY
-  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-  : undefined;
+import config from '@/lib/firebase/config';
 
 if (!admin.apps.length) {
-  if (
-    process.env.FIREBASE_PROJECT_ID &&
-    process.env.FIREBASE_CLIENT_EMAIL &&
-    privateKey
-  ) {
+  if (config.projectId && config.clientEmail && config.privateKey) {
     try {
       admin.initializeApp({
         credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey,
+          projectId: config.projectId,
+          clientEmail: config.clientEmail,
+          privateKey: config.privateKey,
         }),
       });
     } catch (error: any) {
       console.error('Firebase admin initialization error', error.stack);
-      throw new Error('Firebase admin initialization failed.');
     }
   } else {
-    // We can't initialize without credentials, so we'll throw an error.
-    throw new Error('Firebase credentials are not set in the environment.');
+    console.error('Firebase credentials are not set in the environment.');
   }
 }
 
